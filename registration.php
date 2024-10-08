5734c328b464defaefc086b26a830c6d5af93172
giorgi-333 <giorgienuqidze2000@gmail.com>
2024-10-08 22:41:49 +0400

    Added forms#2

--- registration.php
+++ registration.php
@@ -7,7 +7,9 @@
 <body>
     <?php include("./header.php"); ?>
     <h1>გაიარეთ რეგისტრაცია</h1>
-    <form method="post" action="form.php">
+    <!-- action="form.php" -->
+    <form method="post">
+        <input type="text" name="fullName" placeholder="სახელი და გვარი"> <br> <br>
         <input type="text" name="gmail" placeholder="მეილი"> <br> <br>
         <input type="text" name="paswrod" placeholder="პაროლი"> <br> <br>
         <button type="submit">რეგისტრაცია</button>
@@ -15,3 +17,14 @@
     </form>
 </body>
 </html>
+
+<?php
+
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $fullName = $_POST["fullName"];
+    $gmail = $_POST["gmail"];
+    $paswrod = $_POST["paswrod"];
+
+    print "სახელი გვარი: $fullName <br> მეილი: $gmail <br> პაროლი: $paswrod";
+}
