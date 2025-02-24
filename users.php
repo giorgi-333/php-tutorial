d0a0aa326c870c9342ed326f81317eb10f70a33e
giorgi-333 <giorgienuqidze2000@gmail.com>
2025-02-24 10:14:11 +0400

    #20 tutorial (more about SELECT)

--- users.php
+++ users.php
@@ -3,13 +3,14 @@
 // სერვერის სახელი, მომხარებელი, პაროლი, ბაზის სახელი
 $connect = new mysqli("localhost","coding","123","php_tutorial");
 
-$sql = "SELECT * FROM users";
+$sql = "SELECT `fullName`,`gmail` FROM users WHERE `gmail`='giorgi@gmail.com'";
 $result = $connect->query($sql);
+$connect->close();
 
 if($result->num_rows > 0) {
 
     while($row = $result->fetch_assoc()) {
-        echo $row["fullName"] . ' ' . $row["gmail"] . ' ' . $row["paswrod"] . '<br>';
+        echo $row["fullName"] . ' ' . $row["gmail"] . '<br>';
     }
     
 } else {
